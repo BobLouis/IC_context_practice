1e1efd66949ad26d0882d55cc732eaff16e14262
BobLouis <bob900316@gmail.com>
2022-01-18 23:03:24 +0800

    2005 half solved

--- 2005/cs.v
+++ 2005/cs.v
@@ -9,44 +9,56 @@ module CS(Y,
     
     input clk, reset;
     input [7:0] X;
-    output [9:0] Y;
-    
-    reg [7:0]x_reg[9:0];
-    reg [7:0]x_reg_tmp[8:0];
-    reg [9:0]x_avg;
-    reg [9:0]y_reg[1:0];
+    output reg [9:0] Y;
+    wire [12:0]y_tmp;
+    reg [12:0]x_reg[9:0];
+    wire [9:0]x_reg_tmp[9:0];
+    wire [9:0]x_avg;
     reg [3:0]cnt;
-    reg [7:0]dif;
     integer i;
-    wire [7:0]a,b,c,d,e,f,g,x_appr;
+    wire [9:0]a,b,c,d,e,f,g,x_appr;
     
     always @(posedge clk or posedge reset) begin
         
         if (reset)begin
-            for(i = 9;i > 0 ;i = i+1)begin
+            for(i = 0;i < 10 ;i = i+1)begin
                 x_reg[i] <= 0;
             end
             cnt <= 0;
         end
         else begin
             x_reg[9] <= X; //load data
-            for(i = 0;i < 9;i = i+1)begin
+            for(i = 0; i < 9; i = i+1)begin
                 x_reg[i] <= x_reg[i+1];
             end
-            if (cnt<10)begin
+            if (cnt<4'd9)begin
                 cnt <= cnt + 4'd1;
-            end
-            else if (cnt == 4'd9)begin
-                x_avg <= (x_reg[0]+x_reg[1]+x_reg[2]+x_reg[3]+x_reg[4]+x_reg[5]+x_reg[6]+x_reg[7]+x_reg[8])/9;
-                for(i = 0;i < 9;i = i+1)begin
-                    x_reg_tmp[i] <= (x_reg[i] < x_avg)? x_reg[i]:0;
-                end
             end
             else begin
                 cnt <= cnt;
             end
         end
     end
+
+    always @(negedge clk) begin
+        Y <= y_tmp[12:3];
+    end
+
+    // for(i = 0;i < 9;i = i+1)begin
+    //     assign x_reg_tmp[i] = (x_reg[i] < x_avg)? x_reg[i]:0;
+    // end
+
+    assign x_reg_tmp[0] = (x_reg[1] < x_avg)? x_reg[1]:0;
+    assign x_reg_tmp[1] = (x_reg[2] < x_avg)? x_reg[2]:0;
+    assign x_reg_tmp[2] = (x_reg[3] < x_avg)? x_reg[3]:0;
+    assign x_reg_tmp[3] = (x_reg[4] < x_avg)? x_reg[4]:0;
+    assign x_reg_tmp[4] = (x_reg[5] < x_avg)? x_reg[5]:0;
+    assign x_reg_tmp[5] = (x_reg[6] < x_avg)? x_reg[6]:0;
+    assign x_reg_tmp[6] = (x_reg[7] < x_avg)? x_reg[7]:0;
+    assign x_reg_tmp[7] = (x_reg[8] < x_avg)? x_reg[8]:0;
+    assign x_reg_tmp[8] = (x_reg[9] < x_avg)? x_reg[9]:0;
+    
+    assign x_avg = (x_reg[1]+x_reg[2]+x_reg[3]+x_reg[4]+x_reg[5]+x_reg[6]+x_reg[7]+x_reg[8]+x_reg[9])/9;
     assign a = (x_reg_tmp[0]>x_reg_tmp[1]) ? x_reg_tmp[0] : x_reg_tmp[1];
     assign b = (x_reg_tmp[2]>x_reg_tmp[3]) ? x_reg_tmp[2] : x_reg_tmp[3];
     assign c = (x_reg_tmp[4]>x_reg_tmp[5]) ? x_reg_tmp[4] : x_reg_tmp[5];
@@ -57,6 +69,6 @@ module CS(Y,
     assign g = (d>e)?d:e;
     
     assign x_appr = (f>g)?f:g;
-    assign Y      = (x_reg[0]+x_appr+x_reg[1]+x_appr+x_reg[2]+x_appr+x_reg[3]+x_appr+x_reg[4]+x_appr+x_reg[5]+x_appr+x_reg[6]+x_appr+x_reg[7]+x_appr+x_reg[8]+x_appr)>>3;
+    assign y_tmp = x_reg[1]+x_appr+x_reg[2]+x_appr+x_reg[3]+x_appr+x_reg[4]+x_appr+x_reg[5]+x_appr+x_reg[6]+x_appr+x_reg[7]+x_appr+x_reg[8]+x_appr+x_reg[9]+x_appr;
 endmodule
     
