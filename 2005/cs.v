d10eda1b85c5b6a63714a31c9afcf1c811bdd416
BobLouis <bob900316@gmail.com>
2022-01-19 14:26:14 +0800

    formatting

--- 2005/cs.v
+++ 2005/cs.v
@@ -39,39 +39,39 @@ module CS(Y,
             end
         end
     end
-
+    
     always @(negedge clk) begin
-        if(cnt == 4'd9)begin
-          Y <= y_tmp[12:3];
+        if (cnt == 4'd9)begin
+            Y <= y_tmp[12:3];
         end
         else begin
-          Y <= 0;
+            Y <= 0;
         end
     end
-
-    assign x_avg = (x_reg[0]+x_reg[1]+x_reg[2]+x_reg[3]+x_reg[4]+x_reg[5]+x_reg[6]+x_reg[7]+x_reg[8])/9;
-    assign x_reg_tmp[0] = (x_reg[0] <= x_avg)? x_reg[0]:0;
-    assign x_reg_tmp[1] = (x_reg[1] <= x_avg)? x_reg[1]:0;
-    assign x_reg_tmp[2] = (x_reg[2] <= x_avg)? x_reg[2]:0;
-    assign x_reg_tmp[3] = (x_reg[3] <= x_avg)? x_reg[3]:0;
-    assign x_reg_tmp[4] = (x_reg[4] <= x_avg)? x_reg[4]:0;
-    assign x_reg_tmp[5] = (x_reg[5] <= x_avg)? x_reg[5]:0;
-    assign x_reg_tmp[6] = (x_reg[6] <= x_avg)? x_reg[6]:0;
-    assign x_reg_tmp[7] = (x_reg[7] <= x_avg)? x_reg[7]:0;
-    assign x_reg_tmp[8] = (x_reg[8] <= x_avg)? x_reg[8]:0;
+    
+    assign x_avg        = (x_reg[0]+x_reg[1]+x_reg[2]+x_reg[3]+x_reg[4]+x_reg[5]+x_reg[6]+x_reg[7]+x_reg[8])/9;
+    assign x_reg_tmp[0] = (x_reg[0] < = x_avg)? x_reg[0]:0;
+    assign x_reg_tmp[1] = (x_reg[1] < = x_avg)? x_reg[1]:0;
+    assign x_reg_tmp[2] = (x_reg[2] < = x_avg)? x_reg[2]:0;
+    assign x_reg_tmp[3] = (x_reg[3] < = x_avg)? x_reg[3]:0;
+    assign x_reg_tmp[4] = (x_reg[4] < = x_avg)? x_reg[4]:0;
+    assign x_reg_tmp[5] = (x_reg[5] < = x_avg)? x_reg[5]:0;
+    assign x_reg_tmp[6] = (x_reg[6] < = x_avg)? x_reg[6]:0;
+    assign x_reg_tmp[7] = (x_reg[7] < = x_avg)? x_reg[7]:0;
+    assign x_reg_tmp[8] = (x_reg[8] < = x_avg)? x_reg[8]:0;
     
     
     assign a = (x_reg_tmp[0]>x_reg_tmp[1]) ? x_reg_tmp[0] : x_reg_tmp[1];
     assign b = (x_reg_tmp[2]>x_reg_tmp[3]) ? x_reg_tmp[2] : x_reg_tmp[3];
     assign c = (x_reg_tmp[4]>x_reg_tmp[5]) ? x_reg_tmp[4] : x_reg_tmp[5];
     assign d = (x_reg_tmp[6]>x_reg_tmp[7]) ? x_reg_tmp[6] : x_reg_tmp[7];
-
+    
     assign e = (x_reg_tmp[8]>a) ? x_reg_tmp[8] : a;
     
     assign f = (b>c)?b:c;
     assign g = (d>e)?d:e;
     
     assign x_appr = (f>g)?f:g;
-    assign y_tmp = x_reg[0]+x_appr+x_reg[1]+x_appr+x_reg[2]+x_appr+x_reg[3]+x_appr+x_reg[4]+x_appr+x_reg[5]+x_appr+x_reg[6]+x_appr+x_reg[7]+x_appr+x_reg[8]+x_appr;
+    assign y_tmp  = x_reg[0]+x_appr+x_reg[1]+x_appr+x_reg[2]+x_appr+x_reg[3]+x_appr+x_reg[4]+x_appr+x_reg[5]+x_appr+x_reg[6]+x_appr+x_reg[7]+x_appr+x_reg[8]+x_appr;
 endmodule
     
