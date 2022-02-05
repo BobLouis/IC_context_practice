bc85f772e5a3783ff1b033b4bc92922d43ae5520
BobLouis <bob900316@gmail.com>
2022-02-05 11:30:18 +0800

    2006 up

--- 2006/sol.v
+++ 2006/sol.v
@@ -1,8 +1,16 @@
+`define is_upper(x,y) ()
+
 module triangle (clk, reset, nt, xi, yi, busy, po, xo, yo);
    input clk, reset, nt;
    input [2:0] xi, yi;
    output busy, po;
    output [2:0] xo, yo;
+
+   reg [2:0] x0, x1, x2, y0, y1, y2;
+   wire right;
+   
+
+
   
 
    
