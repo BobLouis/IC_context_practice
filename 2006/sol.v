05ed860ddd955595dbd24fc0d74182e23f3d88be
BobLouis <bob900316@gmail.com>
2022-02-18 12:21:29 +0800

    add history

--- 2006/sol.v
+++ 2006/sol.v
@@ -1,17 +1,32 @@
 `define is_upper(x,y) ()
 
-module triangle (clk, reset, nt, xi, yi, busy, po, xo, yo);
-   input clk, reset, nt;
-   input [2:0] xi, yi;
-   output busy, po;
-   output [2:0] xo, yo;
-
-   reg [2:0] x0, x1, x2, y0, y1, y2;
-   wire right;
-   
-
-
-  
-
-   
+module triangle (clk,
+                 reset,
+                 nt,
+                 xi,
+                 yi,
+                 busy,
+                 po,
+                 xo,
+                 yo);
+    input clk, reset, nt;
+    input [2:0] xi, yi;
+    output busy, po;
+    output [2:0] xo, yo;
+    
+    reg [2:0] x0, x1, x2, y0, y1, y2;
+    reg [2:0] state;
+    wire right;
+    
+    assign right = (x2>x1)?1:0;
+    
+    
+    always @(posedge clk) begin
+        
+    end
+    
+    
+    
+    
+    
 endmodule
--- 2015/icc2015cb_pre/ex.v
+++ 2015/icc2015cb_pre/ex.v
@@ -1,56 +1,64 @@
 `define abs(a,b) ((a>b)? a-b:b-a)
 `define is_in_circle(j) \
-    ({1'b0, {3'd0,`abs(x,cx[j])}*abs(x,cx[j]) + {1'b0, {3'd0,`abs(y,cy[j])}*`abs(y,cy[j])}} <= {3'd0,cr[j]}*cr[j])
+({1'b0, {3'd0,`abs(x,cx[j])}*abs(x,cx[j]) + {1'b0, {3'd0,`abs(y,cy[j])}*`abs(y,cy[j])}} <= {3'd0,cr[j]}*cr[j])
 
-module SET ( clk , rst, en, central, radius, mode, busy, valid, candidate );
-
-input clk, rst;
-input en;
-input [23:0] central;
-input [11:0] radius;
-input [1:0] mode;
-output reg busy;
-output reg valid;
-output reg [7:0] candidate;
-
-//reg [6:0] square[0:8];
-reg [3:0] cx[0:2];
-reg [3:0] cy[0:2];
-reg [3:0] cr[0:2];
-
-reg [3:0] x,y;
-integer i;
-
-always @(posedge clk or posedge rst) begin
-    if (rst)begin
-        busy <= 0;
-        valid <= 0;
-        candidate <= 0;
-        x <= 0;
-        y <= 0;
-        for (i = 0; i < 3; i = i + 1)begin
-            cx[i] <= 0;
-            cy[i] <= 0;
-            cr[i] <= 0;
-        end
-        /*for(i = 0; i <= 8; i = i + 1)
-            square[i] <= i*i;*/
-    end
-    else begin
-        if (en) begin
-            x <= 1;
-            y <= 1;
-            busy <= 1;
-            valid <= 0;
+module SET (clk,
+            rst,
+            en,
+            central,
+            radius,
+            mode,
+            busy,
+            valid,
+            candidate);
+    
+    input clk, rst;
+    input en;
+    input [23:0] central;
+    input [11:0] radius;
+    input [1:0] mode;
+    output reg busy;
+    output reg valid;
+    output reg [7:0] candidate;
+    
+    //reg [6:0] square[0:8];
+    reg [3:0] cx[0:2];
+    reg [3:0] cy[0:2];
+    reg [3:0] cr[0:2];
+    
+    reg [3:0] x,y;
+    integer i;
+    
+    always @(posedge clk or posedge rst) begin
+        if (rst)begin
+            busy      <= 0;
+            valid     <= 0;
             candidate <= 0;
+            x         <= 0;
+            y         <= 0;
             for (i = 0; i < 3; i = i + 1)begin
-                cx[i] <= central[23-i*8 -: 4];
-                cy[i] <= central[19-i*8 -: 4];
-                cr[i] <= radius[11-i*4 -: 4];
+                cx[i] <= 0;
+                cy[i] <= 0;
+                cr[i] <= 0;
             end
+            /*for(i = 0; i < = 8; i = i + 1)
+             square[i] <= i*i;*/
         end
-
-        else if (busy) begin
+        else begin
+            if (en) begin
+                x         <= 1;
+                y         <= 1;
+                busy      <= 1;
+                valid     <= 0;
+                candidate <= 0;
+                for (i = 0; i < 3; i = i + 1)begin
+                    cx[i] <= central[23-i*8 -: 4];
+                    cy[i] <= central[19-i*8 -: 4];
+                    cr[i] <= radius[11-i*4 -: 4];
+                end
+            end
+            
+            else if (busy) begin
             if (valid) begin
                 busy <= 0;
             end
@@ -64,13 +72,13 @@ always @(posedge clk or posedge rst) begin
                     end
                     2 :begin
                         if (`is_in_circle(0)^`is_in_circle(1)) candidate <= candidate + 1;
-                    end 
-
+                    end
+                    
                 endcase
                 if (y == 8)begin
                     y <= 1;
                     if (x == 8)begin
-                        x <= 1;
+                        x     <= 1;
                         valid <= 1;
                     end
                     else x <= x + 1;
@@ -80,6 +88,6 @@ always @(posedge clk or posedge rst) begin
         end
         else valid <= 0;
     end
-end
-
+    end
+    
 endmodule
