356eb519d3f89ecf010b41c31ac0b154f3619782
BobLouis <bob900316@gmail.com>
2022-03-13 00:41:42 +0800

    FFT pass

--- 2013/E_ICC2013_preliminary_univ_fpga/FFT_PE/FFT_PE.v
+++ 2013/E_ICC2013_preliminary_univ_fpga/FFT_PE/FFT_PE.v
@@ -12,15 +12,16 @@ module FFT_PE(
 input clk, rst; 		 
 input signed [31:0] a, b;
 input [2:0] power;
-input ab_valid;		
-output [31:0] fft_a, fft_b;
-output fft_pe_valid;
+input ab_valid;		 
+output reg[31:0] fft_a, fft_b;
+output reg fft_pe_valid;
 
-
+	reg signed [31:0]a_tmp_real,a_tmp_img,b_tmp_real,b_tmp_img;
 	reg [2:0]state, next_state;
-	reg [31:0]W_real,W_img;
-	reg [31:0]data_a,data_b;
-
+	reg pe_flag;
+	reg signed[31:0]W_real,W_img;
+	reg signed[31:0]a_real,a_img,b_real,b_img;
+	
 
 	parameter IDLE = 3'd0;
 	parameter READ = 3'd1;
@@ -30,6 +31,12 @@ output fft_pe_valid;
 	always @(posedge clk or posedge rst) begin
 		if(rst == 1) state <= IDLE;
 		else state <= next_state;
+	end
+
+	always @(posedge clk) begin
+		if(ab_valid) next_state <= READ;
+		else if(pe_flag) next_state <= WRITE;
+		else next_state <= IDLE;
 	end
 
 	always@(*)begin
@@ -47,18 +54,34 @@ output fft_pe_valid;
 
 	always @(posedge clk) begin
 		if(ab_valid)begin
-			data_a <= a[31:16];
-			data_b <= a[15:0];
-			data_c <= b[31:16];
-			data_d <= b[15:0];
-		end		
+			a_real <= a[31:16];
+			a_img <= a[15:0];
+			b_real <= b[31:16];
+			b_img <= b[15:0];
+			pe_flag <= 1;
+		end
+		else 
+		begin
+			pe_flag <= 0;		
+		end
+	end
+
+	always @(negedge clk) begin
+		if(pe_flag)
+		begin
+			fft_pe_valid <= 1;
+			fft_a <= {a_tmp_real[15:0],a_tmp_img[15:0]};
+			fft_b <= {b_tmp_real[31:16],b_tmp_img[31:16]};
+
+		end
+		else fft_pe_valid<= 0;
 	end
 
 	always@(*)begin
-		if(fft_pe_valid)begin
-			fft_a = {data_a+data_c,data_b+data_d};
-			fft_b = {(data_a-data_c)*W_real+(data_d-data_b)*W_img,(data_a-data_c)*W_img+(data_b-data_d)*W_real};
-		end
+		a_tmp_real = a_real+b_real;
+		a_tmp_img = a_img+b_img;
+		b_tmp_real = (a_real-b_real)*W_real+(b_img-a_img)*W_img;
+		b_tmp_img = (a_real-b_real)*W_img+(a_img-b_img)*W_real;
 	end
 endmodule
 
