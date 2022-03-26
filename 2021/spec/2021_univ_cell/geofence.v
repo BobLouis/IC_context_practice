4fda456c445cd4890339c7ff846aa7c9f046284a
BobLouis <bob900316@gmail.com>
2022-03-26 21:02:36 +0800

    2020 up

---
+++ 2021/spec/2021_univ_cell/geofence.v
@@ -0,0 +1,44 @@
+module geofence ( clk,reset,X,Y,valid,is_inside);
+input clk;
+input reset;
+input [9:0] X;
+input [9:0] Y;
+output valid;
+output is_inside;
+//reg valid;
+//reg is_inside;
+
+always @(posedge clk or posedge reset) begin
+
+end
+
+always @(posedge clk or posedge reset) begin
+%{}
+end
+
+always @(posedge clk or posedge reset) begin
+${}
+end
+
+always @(posedge clk or posedge reset) begin
+
+end
+
+always @(posedge clk or posedge reset) begin
+    
+end
+
+if()begin
+
+end
+else begin
+end
+
+if()begin
+
+end
+else begin
+
+end
+endmodule
+
