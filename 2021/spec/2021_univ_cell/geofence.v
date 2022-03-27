8a3a189625b8e1442a3324d9a3bcc1fc17d2c6ec
BobLouis <bob900316@gmail.com>
2022-03-27 13:09:08 +0800

    up

--- 2021/spec/2021_univ_cell/geofence.v
+++ 2021/spec/2021_univ_cell/geofence.v
@@ -1,4 +1,4 @@
-`define OUTER(Ax,Ay,Bx,By) (((Ax*By) > (Ay*Bx)) ? 1:0)
+`define OUTER(Ax,Ay,Bx,By) (((Ax*By) - (Ay*Bx)) >0 ? 1:0)
 
 module geofence ( clk,reset,X,Y,valid,is_inside);
 input clk;
@@ -23,6 +23,10 @@ reg [9:0]loc_x[0:5];
 reg [9:0]loc_y[0:5];
 reg [5:0]judge;
 wire outer_tmp;
+wire outer_set;
+wire signed[19:0]mul1,mul2;
+
+
 
 //set
 reg [2:0]cmp1,cmp2;
@@ -90,6 +94,10 @@ always @(posedge clk or posedge reset) begin
             cmp2 <= cmp2 + 3'd1;
         end
     end
+    else begin
+        cmp1 <= 3'd1;
+        cmp2 <= 3'd2;
+    end
 end
 
 
@@ -101,6 +109,10 @@ assign v2_x = loc_x[cmp2] - loc_x[0];
 assign v2_y = loc_y[cmp2] - loc_y[0];
 
 
+
+assign outer_set = (`OUTER(v1_x,v1_y,v2_x,v2_y));
+assign mul1 = v1_x*v2_y;
+assign mul2 = v1_y*v2_x;
 
 //DATA INPUT AND SET
 always@(posedge clk)begin
@@ -114,8 +126,8 @@ always@(posedge clk)begin
             loc_y[counter-1] <= Y;
         end
     end
-    else if(next_state == SET) begin
-        if( (`OUTER(v1_x,v1_y,v2_x,v2_y)) == 0) begin
+    else if(next_state == SET || state == SET) begin
+        if( outer_set == 0) begin
             //swap
             loc_x[cmp1] <= loc_x[cmp2];
             loc_x[cmp2] <= loc_x[cmp1];
@@ -137,7 +149,7 @@ always @(posedge clk or posedge reset) begin
     end
 end
 
-assign  outer_tmp = `OUTER(target_x, target_y, loc_x[cal_cnt], loc_y[cal_cnt]);
+assign  outer_tmp = `OUTER(target_x - loc_x[cal_cnt], target_y - loc_y[cal_cnt], loc_x[cal_cnt+1] - loc_x[cal_cnt], loc_y[cal_cnt+1]-loc_y[cal_cnt]);
 
 
 always@(posedge clk or posedge reset)begin
